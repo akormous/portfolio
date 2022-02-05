827948729f332b7822d856e0a6e6fdf694e0639c
akormous <iamakshatchauhan@gmail.com>
2022-02-06 00:56:07 +0530

    Update Job data to json + fixing stuff

--- ui/src/components/JobExperience.js
+++ ui/src/components/JobExperience.js
@@ -1,9 +1,11 @@
 import { Container, makeStyles, Typography, Paper, List, ListItem } from '@material-ui/core';
 import React from 'react';
 import {Timeline, TimelineDot, TimelineItem, TimelineSeparator, TimelineConnector, TimelineContent, TimelineOppositeContent} from '@material-ui/lab';
-import FlightSharpIcon from '@material-ui/icons/FlightSharp';
-import BarChartSharpIcon from '@material-ui/icons/BarChartSharp';
 import Heading from './Heading';
+import { getIconFromKey } from '../utility/common';
+
+/* import job exp data from jobexperience.json */
+import jobexperience from './data/jobexperience.json';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -56,112 +58,38 @@ export default function JobExperience(props) {
             <Container maxWidth='md'>
                 <Heading content="Experience" />
                 <Timeline align="left" position="right">
-                    <TimelineItem>
-                        <TimelineOppositeContent className={styles.oppositeContent} />
-                        <TimelineSeparator>
-                        <TimelineDot color="primary">
-                            <FlightSharpIcon />
-                        </TimelineDot>
-                        <TimelineConnector />
-                        </TimelineSeparator>
-                        <TimelineContent>
-                        <Typography variant="body1" className={styles.companyname}>
-                        Amadeus IT Group
-                        </Typography>     
-                        <Typography variant="body1" className={styles.designation}>
-                        Software Engineer
-                        </Typography>
-                        <Typography variant="caption">
-                        Aug 2021 - Present
-                        </Typography>
-                        <List disablePadding>
-                            <ListItem disableGutters>
-                                <Typography align='justify' variant="body2">
-                                Working as a C++ Developer in PAYMENTS team.
-                                </Typography>
-                            </ListItem>
-                            <ListItem disableGutters>
-                                <Typography align='justify' variant="body2">
-                                Contributing to PCI-DSS compliance of Amadeus applications.
-                                </Typography>
-                            </ListItem>
-                        </List>
-                        
-                        </TimelineContent>
-                    </TimelineItem>
-                    <TimelineItem>
-                        <TimelineOppositeContent className={styles.oppositeContent} />
-                        <TimelineSeparator>
-                        <TimelineDot color="primary">
-                            <FlightSharpIcon />
-                        </TimelineDot>
-                        <TimelineConnector />
-                        </TimelineSeparator>
-                        <TimelineContent>
-                        <Typography variant="body1" className={styles.companyname}>
-                        Amadeus IT Group
-                        </Typography>     
-                        <Typography variant="body1" className={styles.designation}>
-                        Software Engineer Intern
-                        </Typography>
-                        <Typography variant="caption">
-                        Jan 2021 - Jun 2021
-                        </Typography>
-                        <List disablePadding>
-                            <ListItem disableGutters>
-                                <Typography align='justify' variant="body2">
-                                Built a platform that automates Amadeus Payment Test Scenarios.
-                                </Typography>
-                            </ListItem>
-                            <ListItem disableGutters>
-                                <Typography align='justify' variant="body2">
-                                Reduced analysis time and error investigation time by 50%
-                                </Typography>
-                            </ListItem>
-                            <ListItem disableGutters>
-                                <Typography align='justify' variant="body2">
-                                Developed using React, Redux, and Express.js
-                                </Typography>
-                            </ListItem>
-                        </List>
-                        </TimelineContent>
-                    </TimelineItem>
-                    <TimelineItem>
-                        <TimelineOppositeContent className={styles.oppositeContent} />
-                        <TimelineSeparator>
-                        <TimelineDot color="primary">
-                            <BarChartSharpIcon />
-                        </TimelineDot>
-                        </TimelineSeparator>
-                        <TimelineContent>
-                        <Typography variant="body1" className={styles.companyname}>
-                            CL Educate Ltd.
-                        </Typography>
-                        <Typography variant="body1" className={styles.designation}>
-                        Data Science Intern
-                        </Typography>
-                        <Typography variant="caption">
-                        May 2020 - Jun 2020
-                        </Typography>
-                        <List disablePadding>
-                            <ListItem disableGutters>
-                                <Typography align='justify' variant="body2">
-                                Applied machine learning algorithms on stock market data to gain insights such as Beta value of a stock, future stock prices, Efficient Frontier, and correlation between stocks.
-                                </Typography>
-                            </ListItem>
-                            <ListItem disableGutters>
-                                <Typography align='justify' variant="body2">
-                                Identified trade calls using Bollinger Bands and Simple Moving Averages
-                                </Typography>
-                            </ListItem>
-                            <ListItem disableGutters>
-                                <Typography align='justify' variant="body2">
-                                Developed using Python, Numpy, Pandas, Matplotlib, Sci-kit learn
-                                </Typography>
-                            </ListItem>
-                        </List>
-                        </TimelineContent>
-                    </TimelineItem>
+                    {jobexperience.map((job) => (
+                        <TimelineItem>
+                            <TimelineOppositeContent className={styles.oppositeContent} />
+                            <TimelineSeparator>
+                            <TimelineDot color="primary">
+                                {getIconFromKey(job.iconKey)}
+                            </TimelineDot>
+                                {job.isLastObject ? <></> : <TimelineConnector />}
+                            </TimelineSeparator>
+                            <TimelineContent>
+                            <Typography variant="body1" className={styles.companyname}>
+                            {job.company}
+                            </Typography>     
+                            <Typography variant="body1" className={styles.designation}>
+                            {job.designation}
+                            </Typography>
+                            <Typography variant="caption">
+                            {job.tenure}
+                            </Typography>
+                            <List disablePadding>
+                                {job.description.map((line) => (
+                                    <ListItem disableGutters>
+                                        <Typography align='justify' variant="body2">
+                                        {line}
+                                        </Typography>
+                                    </ListItem>
+                                ))}
+                            </List>
+                            
+                            </TimelineContent>
+                        </TimelineItem>
+                    ))}
                 </Timeline>
             </Container>
         </Paper>
